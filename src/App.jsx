a87f1d0e6340d645e67ca8fae6bb7376e7baffb3
Gauravvgithub <gauravraj7011@gmail.com>
2025-01-27 16:04:09 +0530

    back to top button add

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import MyWork from './Components/MyWork/MyWork'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
 import CustomCursor from './Components/CustomCursorPointer/CustomCursorPointer'
+import BackToTop from './Components/BackToTop/BackToTop'
 
 const App = () => {
   return (
     <div>
       <CustomCursor/>
       <Navbar/>
+      <BackToTop/>
       <Hero/>
       <About/>
       <Services/>
